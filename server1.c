fc4cf8480129d127e5c2cec41da08b3790ec8ad1
chandan kumar <116447157+NBSPRG@users.noreply.github.com>
2023-02-18 11:17:24 +0530

    Add files via upload

--- server1.c
+++ server1.c
@@ -1,84 +1,120 @@
+// Server side program that sends
+// a 'hi client' message
+// to every client concurrently
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h> // for close
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// PORT number
+#define PORT 4444
 
 int main()
 {
-	int error_check;
-	char server_message[256];
-	socklen_t client;
-	// create the server socket
-	int server_socket;
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	// Server socket id
+	int sockfd, ret;
 
-	// define the server address
-	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9001);
-	server_address.sin_addr.s_addr = INADDR_ANY;
+	// Server socket address structures
+	struct sockaddr_in serverAddr;
 
-	// client server address to get messages
-	struct sockaddr_in client_address;
-	client = sizeof(client_address);
+	// Client socket id
+	int clientSocket;
 
-	// bind the socket to our specified IP and port
-	error_check = bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
-	if (error_check == -1)
-	{
-    	printf("Some error in binding\n\n");
-    	exit(1);
+	// Client socket address structures
+	struct sockaddr_in cliAddr;
+
+	// Stores byte size of server socket address
+	socklen_t addr_size;
+
+	// Child process id
+	pid_t childpid;
+
+	// Creates a TCP socket id from IPV4 family
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// Error handling if socket id is not valid
+	if (sockfd < 0) {
+		printf("Error in connection.\n");
+		exit(1);
 	}
 
-	error_check = listen(server_socket, 5); // here 5 can be at max client connected to server at a time
-	if (error_check == -1)
-	{
-    	printf("Some error in listening\n\n");
-    	exit(1);
+	printf("Server Socket is created.\n");
+
+	// Initializing address structure with NULL
+	memset(&serverAddr, '\0',
+		sizeof(serverAddr));
+
+	// Assign port number and IP address
+	// to the socket created
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+
+	// 127.0.0.1 is a loopback address
+	serverAddr.sin_addr.s_addr
+		= inet_addr("127.0.0.1");
+
+	// Binding the socket id with
+	// the socket structure
+	ret = bind(sockfd,
+			(struct sockaddr*)&serverAddr,
+			sizeof(serverAddr));
+
+	// Error handling
+	if (ret < 0) {
+		printf("Error in binding.\n");
+		exit(1);
 	}
 
-	int client_socket;
-	//first is socket to which client will connect, second is a struct which will get filled up with data from client socket with its address, port etc, if not needed use NULL
-	client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client);
-
-	if (client_socket == -1)
-	{
-    	printf("unexpected error occured\n\n");
+	// Listening for connections (upto 10)
+	if (listen(sockfd, 10) == 0) {
+		printf("Listening...\n\n");
 	}
 
-	while (1)
-	{
-    	error_check = recv(client_socket, server_message, sizeof(server_message), 0);
-    	if (error_check == -1)
-    	{
-        	printf("error in receiving from client\n\n");
-        	exit(1);
-    	}
+	int cnt = 0;
+	while (1) {
 
-    	printf("Client: %s\n", server_message);
-    	printf("Server: ");
+		// Accept clients and
+		// store their information in cliAddr
+		clientSocket = accept(
+			sockfd, (struct sockaddr*)&cliAddr,
+			&addr_size);
 
-    	fgets(server_message, sizeof(server_message), stdin);
+		// Error handling
+		if (clientSocket < 0) {
+			exit(1);
+		}
 
-    	if (strncmp(server_message, "bye", 3) == 0)
-    	{
-        	break;
-    	}
+		// Displaying information of
+		// connected client
+		printf("Connection accepted from %s:%d\n",
+			inet_ntoa(cliAddr.sin_addr),
+			ntohs(cliAddr.sin_port));
 
-    	error_check = send(client_socket, server_message, sizeof(server_message), 0);
-    	if (error_check == -1)
-    	{
-        	printf("error in sending message\n");
-        	exit(1);
-    	}
+		// Print number of clients
+		// connected till now
+		printf("Clients connected: %d\n\n",
+			++cnt);
+
+		// Creates a child process
+		if ((childpid = fork()) == 0) {
+
+			// Closing the server socket id
+			close(sockfd);
+
+			// Send a confirmation message
+			// to the client
+			send(clientSocket, "hi client",
+				strlen("hi client"), 0);
+		}
 	}
 
-	//close the socket
-	close(server_socket);
-
+	// Close the client socket id
+	close(clientSocket);
 	return 0;
 }
 
